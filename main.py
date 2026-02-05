b088bdddc6c624a0d1f0323eac2fef9d77c64a42
amandayaworsky <amandayaworsky@gmail.com>
2026-02-05 18:13:01 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+
+#fetching sequences from NCBI
+#Validating sequence format is correct
+#Calls the main functions
+"""
+#!/usr/bin/env python3
+    """ Overall File Purpose:
+This script coordinates the overall workflow of the program. It  connects sequence loading, ORF detection, ORF comparison, and summary statistics program.
+    """
+
+    """Role in project: 
+
+It works as the control center which calls functions from other modules in the correct order.
+
+Input: 
+DNA sequence file in FASTA format from NCBI
+
+Output: 
+
+Summary of ORFs and repeated ORFs.
+
+High level steps:
+
+Load DNA sequence from a FASTA file
+Identify all possible ORFs in all reading frames.
+Analyze ORFs to find repeated or sequences.
+Generate summary statistics.
+Display results to the user.
+    """
+pass
+
